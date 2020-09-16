86c5c9f7bd5a2d1f1235682a144b16643bc1eb86
chrispaul20191610 <crispaulpolitec2016@gmail.com>
2020-09-15 23:46:52 -0500

    avanze navegacion entre paginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 }
